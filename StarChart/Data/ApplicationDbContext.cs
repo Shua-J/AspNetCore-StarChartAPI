3c410b6fd5e925e2437f77cfbec25bd685d4aa05
Jramos8 <jramos8@rockets.utoledo.edu>
2021-09-25 16:40:50 -0400

    Commit 3

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace StarChart.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<CelestialObject> CelestialObjects { get; set; }
-
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
