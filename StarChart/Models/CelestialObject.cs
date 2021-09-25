9b0799c7df4e6991e52f7dbc9ce3587a9d4b08df
Jramos8 <jramos8@rockets.utoledo.edu>
2021-09-25 16:31:54 -0400

    Commit 2

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        int Id { get; set; }
+        [Required]
+        string Name { get; set; }
+        int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        List<CelestialObject> Satellites { get; set; }
+        TimeSpan OrbitalPeriod { get; set; }
+
+    }
+}
